f4969a986d47d7717b5082cf3ea691be431a1a76
kevinciro <70342180+kevinciro@users.noreply.github.com>
2022-08-31 15:34:33 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,85 +1,120 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-//prueba comentario
+#include <fstream>
+#include <list>
+
 
 using namespace std;
 
+double S = 1000.0; //tension puntos extremos
+double E = 3.0e7;//modulo elasticidad
+double I = 625.0; //momento inercia
+double q = 100.0; //intensidad carga
+double l = 120.0; //longitud
 
-//ecuación diferencial matemática. 
-/*double f_y(double x, double w_i, double t){
-    return( -(1./8.)*t);
-}
 
-double f_yp(double x, double w_i, double t){
-    return -(1./8.)*(w_i);
-}
-
-double f(double x, double w_i, double t){
-    return (1./8.)*( 32 + pow(x,3) - (w_i)*t );
-}*/
-
-//ecuaciones del péndulo simple no lineales. 
-const float g=9.8;
-const float l=1;
 double f_y(double x, double w_i, double t){
-    return( -(g/l)*cos(x) );
+    return -18*w_i*pow(x,-5);
 }
 
 double f_yp(double x, double w_i, double t){
-    return 0;
+    return 2*pow(x,-3)*t;
 }
 
 double f(double x, double w_i, double t){
-    return (-(g/l)*sin(x) );
+    return pow(t,2)*pow(x,-3) - 9*pow(w_i,2)* pow(x,-5) + 4*x;
 }
 
-//ecuación diferencial de la viga 
-const float g=9.8;
-const float l=1;
-double f_y(double x, double w_i, double t){
-    return( -(g/l)*cos(x) );
-}
+//double f_y(double x, double w_i, double t){
+//    return( pow((1. + pow(t,2)), 3./2.) * S/(E*I) );
+//}
+//
+//double f_yp(double x, double w_i, double t){
+//    return 3*( (S/(E*I))*w_i + (x-l)*(q*x)/(2*E*I) )*pow((1. + pow(t,2)), 1./2.)*t;
+//}
+//
+//double f(double x, double w_i, double t){
+//    return pow((1. + pow(t,2)), 3./2.)*( (S/(E*I))*w_i + (x-l)*(q*x)/(2*E*I) );
+//}
 
-double f_yp(double x, double w_i, double t){
-    return 0;
-}
+//Ejemplo del libro
+//double f_y(double x, double w_i, double t){
+//    return( -(1./8.)*t);
+//}
+//
+//double f_yp(double x, double w_i, double t){
+//    return -(1./8.)*(w_i);
+//}
+//
+//double f(double x, double w_i, double t){
+//    return (1./8.)*( 32 + pow(x,3) - (w_i)*t );
+//}
 
-double f(double x, double w_i, double t){
-    return (-(g/l)*sin(x) );
-}
-///////////////////////////////////////////////////////// se define el método. 
+//ecuaciones del péndulo simple no lineales.
+//const double g=9.8;
+//const double l=1.0;
+//double f_y(double x, double w_i, double t){
+//    return( -(g/l)*cos(x) );
+//}
+//
+//double f_yp(double x, double w_i, double t){
+//    return 0;
+//}
+//
+//double f(double x, double w_i, double t){
+//    return (-(g/l)*sin(x) );
+//}
+
 vector<vector<double>> dif_fin_nl(
+//        [a,b] es el intervlo
         double a,
         double b,
+//        alfa y beta son las condiciones de frontera en el intervalo
         double alpha,
         double beta,
+//        N es el numero de intervalos en los que se dividira [a,b]
         int N,
+//        M es el numero de iteraciones maxima
         int M,
+//        tolerancia deseada
         double tol){
+
+
     double x;
+//    t es la derivada
     double t;
-    std::vector<double> A(N+1);
-    std::vector<double> B(N+1);
-    std::vector<double> C(N+1);
-    std::vector<double> D(N+1);
-    std::vector<double> L(N+1);
-    std::vector<double> U(N+1);
-    std::vector<double> Z(N+1);
+
+    std::vector<double> A;
+    std::vector<double> B;
+    std::vector<double> C;
+    std::vector<double> D;
+    std::vector<double> L;
+    std::vector<double> U;
+    std::vector<double> Z;
     std::vector<double> V(N+1);
-    std::vector<double> X(N+1);
+    std::vector<double> X;
     vector<vector<double>> respuesta;
 
 //    Paso 1
+//    las condiciones iniciales se guardan en el vector W
     double h = (b - a) / (N + 1);
-    std::vector<double> W(N+1);
-    W[0] = alpha;
-    W[N+1] = beta;
+    std::vector<double> W;
+    W.push_back(alpha); //posicion 0
 
 //  Paso 2
-    for (int i = 0; i < N; ++i) {
-        W[i]=alpha + (i * h * (beta - alpha) / (b - a));
+    for (int i = 1; i <= N; ++i) {
+        W.push_back(alpha + (i * h * (beta - alpha) / (b - a))); //posicion 1 hasta la N-1
     }
+
+    W.push_back(beta); // posicion N+1
+
+
+//    for ( double w: W){
+//        cout << w << " ";
+//    }
+//    cout << endl;
+
 
 //    Paso 3
     int k = 1;
@@ -89,96 +124,131 @@ vector<vector<double>> dif_fin_nl(
 //        Paso 5
         x = a + h;
         t = (W[2] - alpha) / (2*h);
-        A[0] = 2. + pow(h,2)* f_y(x, W[1],t);
-        B[0] = -1 + h * 0.5 * f_yp(x, W[1],t);
-        D[0] = -(2*W[1] - W[2] - alpha + pow(h,2)* f(x, W[1],t));
+        A.push_back(2. + pow(h,2) * f_y(x, W[1],t)); //posicion 0
+        B.push_back(-1 + h * 0.5 * f_yp(x, W[1],t)); //posicion 0
+        D.push_back(-(2*W[1] - W[2] - alpha + pow(h,2)* f(x, W[1],t))); //posicion 0
 
 //        Paso 6
         for(int i=1; i<N; i++){
             x = a + i*h;
             t = (W[i+1] - W[i-1]) / (2*h);
-            A[i] = 2. + pow(h,2)* f_y(x, W[i],t);
-            B[i] = -1 + h * 0.5 * f_yp(x, W[i],t);
-            C[i] = -1 - h * 0.5 * f_yp(x, W[i],t);
-            D[i] = -(2*W[i] - W[i+1] - W[i-1] + pow(h,2)* f(x, W[i],t));
+            A.push_back(2. + pow(h,2)* f_y(x, W[i],t)); //posiciones desde la 1 hasta la N-1
+            B.push_back(-1 + h * 0.5 * f_yp(x, W[i],t)); //posiciones desde la 1 hasta la N-1
+            C.push_back(-1 - h * 0.5 * f_yp(x, W[i],t)); //posiciones desde la 0 hasta la N-2
+            D.push_back(-(2*W[i] - W[i+1] - W[i-1] + pow(h,2)* f(x, W[i],t))); //posiciones desde la 1 hasta la N-1
 
         }
 
 //        Paso 7
         x = b - h;
         t = (beta - W[N-1]) / (2*h);
-        A[N] = 2. + pow(h,2)* f_y(x, W[N],t);
-        C[N] = -1 - h * 0.5 * f_yp(x, W[N],t);
-        D[N] = -(2*W[N] - W[N-1] - beta + pow(h,2)* f(x, W[N],t));
+        A.push_back(2. + pow(h,2)* f_y(x, W[N],t)); // posicion N
+        C.push_back(-1 - h * 0.5 * f_yp(x, W[N],t)); // posicion N-1
+        D.push_back(-(2*W[N] - W[N-1] - beta + pow(h,2)* f(x, W[N],t))); //posicion N
 
 //        Paso 8
-        L[0] = A[0];
-        U[0] = B[0]/A[0];
-        Z[0] = D[0]/L[0];
+        L.push_back(A[0]); //posicion 0
+        U.push_back(B[0]/A[0]); //posicion 0
+        Z.push_back(D[0]/L[0]); //posicion 0
 
 //        Paso 9
         for(int i=1; i<N; i++){
-            L[i] = A[i] - C[i]*U[i-1];
-            U[i] = B[i]/L[i];
-            Z[i] = (D[i] - C[i]*Z[i-1])/L[i];
+            L.push_back(A[i] - C[i]*U[i-1]); //posiciones desde la 1 hasta la N-1
+            U.push_back(B[i]/L[i]); //posiciones desde la 1 hasta la N-1
+            Z.push_back((D[i] - C[i]*Z[i-1])/L[i]); //posiciones desde la 1 hasta la N-1
         }
 
 //        Paso 10
-        L[N] = A[N] - C[N]*U[N-1];
-        Z[N] = (D[N] - C[N]*Z[N-1])/L[N];
+        L.push_back(A[N] - C[N]*U[N-1]); // posicion N
+        Z.push_back((D[N] - C[N]*Z[N-1])/L[N]); // posicion N
 
 //        Paso 11
-        V[N] = Z[N];
-        W[N] = W[N] + V[N];
+        V[N] = Z[N]; // posisicon N
+        W[N] = W[N] + V[N]; // posisicon N, La posicion N+1 siempre sera beta
 
 //        Paso 12
-        for(int i=N; i>0; i--){
-            V[i] = Z[i] - U[i]*V[i+1];
-            W[i] = W[i] + V[i];
+        for(int i=N-1; i>=0; i--){
+            V[i] = Z[i] - U[i]*V[i+1]; // posisicon desde la N-1 hasta la 0
+            W[i] = W[i] + V[i]; // posisicon desde la N-1 hasta la 0
         }
 
 //        Paso 13
         double norma = 0;
-        for(double i: V){
-            norma = norma + pow(V[i],2);
+        for(double i : V){
+            norma += pow(i,2);
         }
         norma = sqrt(norma);
+//        cout << norma << endl;
+
+/*        for ( double v: V){
+            cout << v << " ";
+        }*/
 
         if (norma <= tol){
 //            Paso 14
             for(int i=0; i<=N+1; i++){
-                X[i] = a + i*h;
+                X.push_back(a + i*h);
             }
 //            Paso 15
             respuesta.push_back(X);
             respuesta.push_back(W);
             return respuesta;
         }
+
+        A.clear();
+        B.clear();
+        C.clear();
+        D.clear();
+        L.clear();
+        U.clear();
+        Z.clear();
+
         k++;
     }
 
 //    Paso 16 y 17
     throw "Numero maximo de iteraciones alcanzado";
+
 }
 
 
 int main() {
-    vector<vector<double>> respuestas;
-    respuestas = dif_fin_nl(1,3,17,43./3.,19,100000, 1e-8); //a, b, alpha, beta, N, M, tol.
+    const double PI = 3.1416;
+    try{
+        vector<vector<double>> respuestas;
+//        double a = 0;
+//        double b = PI * sqrt(l/g);
+//    //        alfa y beta son las condiciones de frontera en el intervalo
+//        double alpha = -PI/4;
+//        double beta = PI/4;
+//    //        N es el numero de intervalos en los que se dividira [a,b]
+//        int N = 10;
+//    //        M es el numero de iteraciones maxima
+//        int M = 1000000;
+//    //        tolerancia deseada
+//        double tol = 1e-8;
+//        respuestas = dif_fin_nl(a, b, alpha, beta, N, M, tol);
 
-    cout << respuestas[0].size() << endl;
-    cout << respuestas[1].size() << endl;
+//    Ejemplo del libro
+//        respuestas = dif_fin_nl(1,3,17,43./3.,50,100000, 1e-12); //a, b, alpha, beta, N, M, tol.
 
-    for ( double x: respuestas[0]){
-        cout << x << " ";
+//        respuestas = dif_fin_nl(0, l, 0,0,50,100000, 1e-12); //a, b, alpha, beta, N, M, tol.
+
+        respuestas = dif_fin_nl(1, 2, 0, log(256),50,100000, 1e-12); //a, b, alpha, beta, N, M, tol.
+
+        for ( double x: respuestas[0]){
+            cout << x << ", ";
+        }
+        cout << endl;
+
+        for ( double w: respuestas[1]){
+            cout << w << ", ";
+        }
+        cout << endl;
+    }catch (const char* msg){
+        std::cout<< msg << std::endl;
     }
-    cout << endl;
-
-    for ( double w: respuestas[1]){
-        cout << w << " ";
-    }
-    cout << endl;
-
 
     return 0;
 }
+
